fe8cc4ae5b85414af2a7c78acea5a68eebe8ad73
Georges FORTRY <gfortry@hotmail.com>
2021-07-22 11:44:19 +0200

    contact mail operational

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { backPort } = require('./db-config');
 const skillsRouter = require('./routes/skills');
+const contactRouter = require('./routes/contact');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/skills', skillsRouter);
+app.use('/contact', contactRouter);
 
 app.use('/', (req, res) => {
   res.status(404).send('Route not found! ');
---
+++ routes/contact.js
@@ -0,0 +1,47 @@
+const contactRouter = require('express').Router();
+
+const Joi = require('joi');
+const nodemailer = require('nodemailer');
+
+const contactEmail = nodemailer.createTransport({
+  service: process.env.SERVICE,
+  auth: {
+    user: process.env.CONTACT_EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+contactRouter.post('/', (req, res) => {
+  const { firstname, lastname, structure, phonenumber, email, message } =
+    req.body;
+
+  const { error } = Joi.object({
+    email: Joi.string().email().max(255).required(),
+  }).validate({ email }, { abortEarly: false });
+  const mail = {
+    from: { firstname, lastname },
+    to: process.env.CONTACT_EMAIL,
+    subject: 'Contact Portfolio',
+    html: `<p>Prénom: ${firstname}</p>
+          <p>Nom: ${lastname}</p>
+          <p>Structure: ${structure || 'Non renseignée'}</p>
+          <p>Numéro de téléphone: ${phonenumber || 'Non renseigné'} 
+          <p>Email: ${email}</p>
+          <p>Validation RGPD : Oui</p>
+          <p>Message: ${message}</p>`,
+  };
+
+  if (error) {
+    res.status(422).send({ validationErrors: error.details });
+  } else {
+    contactEmail.sendMail(mail, (err) => {
+      if (err) {
+        res.json({ status: 'Error' });
+      } else {
+        res.json({ status: 'Message sent' });
+      }
+    });
+  }
+});
+
+module.exports = contactRouter;
